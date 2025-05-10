9fd9109a655e0d4a2dcc1288b1ebf1b092d5c87f
UniBreakfast <unibreakfast1@gmail.com>
2025-05-10 09:05:10 +0300

    update project description

--- grok/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-async function connectToMongoDB() {
-  await mongoose.connect('mongodb://localhost:27017/crudEvents')
-  console.log('Connected to MongoDB')
-  return mongoose
-}
-
-module.exports = connectToMongoDB
